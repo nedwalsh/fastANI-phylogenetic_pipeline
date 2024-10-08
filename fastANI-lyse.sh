34fa35f8e1f6fe9cc14f5ae5d7de01391c11f7d6
nedwalsh <113162888+nedwalsh@users.noreply.github.com>
2024-10-08 16:18:46 +1100

    Update fastANI-lyse.sh

--- fastANI-lyse.sh
+++ fastANI-lyse.sh
@@ -16,6 +16,9 @@ if [ ! -d "$output" ]; then
     mkdir "$output"
 fi
 
+for i in "$source_dir/*"; do 
+    echo "$(basename $i | cut -d"_" -f1-2),$(head -n 1 $i | cut -d"," -f1 | cut -d" " -f2-3)"; done > output/species_info.csv
+
 if [ "$num" -gt 50]; then
         src/run_split_analysis.sh $source_dir
 else
