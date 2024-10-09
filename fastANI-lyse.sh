94a324fbcb0e6dd7238e5ead3fbe9b439be52720
nedwalsh <113162888+nedwalsh@users.noreply.github.com>
2024-10-09 14:45:39 +1100

    Update fastANI-lyse.sh

--- fastANI-lyse.sh
+++ fastANI-lyse.sh
@@ -35,7 +35,7 @@ if [ ! -d "${temp}" ]; then
 fi
 
 for i in "$source_dir"*; do 
-    echo "$(basename $i | cut -d"_" -f1-2),$(head -n 1 $i | cut -d"," -f1 | cut -d" " -f2-5)"; done > output/species_info.csv
+    echo "$(basename $i | cut -d"_" -f1-2),$(head -n 1 $i | cut -d"," -f1 | cut -d" " -f2-5)"; done > ${output}/species_info.csv
 
 name="$(dirname "${BASH_SOURCE[0]}")"
 
